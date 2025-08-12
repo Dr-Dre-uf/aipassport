84f76dab3bc8d7f17bc9cf23b18e62d2901f0561
Dr-Dre-uf <deandrecoleman@ufl.edu>
2025-08-12 14:10:55 -0400

    Update streamlit_app.py
    add imports

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install('opencv-python')
 import streamlit as st
 import cv2
 import numpy as np
